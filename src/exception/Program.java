31f3a1e07b4539028c54f1adf2bc8f805072461c
Felipe Frazão <felipe.frazao@outlook.com.br>
2020-05-04 20:26:04 -0300

    Ediçao em tempo real

--- src/exception/Program.java
+++ src/exception/Program.java
@@ -1,5 +1,7 @@
 package exception;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,20 +9,48 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		method3();
+		System.out.println("End of program");
+	}
+
+	public static void method1() {
+
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
-				System.out.println(vect[position]);
-			}
-			catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid position!");
-			}
-			catch (InputMismatchException e) {
-				System.out.println("Input error");
-			}
-			System.out.println("End of program");
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
 		sc.close();
+		System.out.println("***METHOD2 END***");
 	}
 
+	public static void method3() {
+		File file = new File("C:\\Users\\Felipe\\Tarefa 29-04.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
 }
